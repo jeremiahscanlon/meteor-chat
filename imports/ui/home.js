997e0a1564aaa093e1cf3c12625a7ba5e303e5ea
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-06-15 21:06:16 -0400

    new git location

---
+++ imports/ui/home.js
@@ -0,0 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+import './home.html';
+
+Template.home.onCreated(function () {
+    console.log('loaded');
+
+});
+
+Template.home.helpers({
+    'message':function () {
+        return Message.find({},{sort:{created_on: -1}, limit:10}).fetch().reverse();
+    },
+    'userLoggedIn': function()  {
+        return Meteor.user();
+    }
+
+});
+
+Template.home.events({
+    'submit #new-msg': function (event) {
+        event.preventDefault();
+        var message = $("[name='message']").val();
+        Meteor.call('insertMessage', message);
+        setTimeout(function (){
+            $("[name='message']").val('');
+        },100);
+    }
+});
